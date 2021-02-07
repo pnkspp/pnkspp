8f3390ad274a1120e099ec90b30c24bd4e63c4e3
DESKTOP-R4E5JUB\Kreon <renewz.engineering@gmail.com>
2021-02-07 14:28:49 +0700

    Initial stage

---
+++ react-website/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import Counter from './components/Counter' ; 
+import 'bootstrap/dist/css/bootstrap.css'; 
+ReactDOM.render( <App />,document.getElementById('root')
+// ReactDOM.render( <Counter />,document.getElementById('root')
+)
+
+
